05862493e6bb0465709644be7eaae595929a62e0
IvanovKirill975 <kirill707.ivanov@gmail.com>
2022-10-19 20:31:27 +0300

    kira

---
+++ javaScript.js
@@ -0,0 +1,20 @@
+let text = "Whats your name?";
+let number = 100
+
+console.log(text);
+// alert(text + number)
+prompt(text)
+
+// console.log(text_1);
+// text_1 = "Alt tetx";
+
+
+
+
+// console.log(text_1);
+// console.log(text_1);
+// const text = "значение переменной изменять нельзя"
+// let text_1 = "это способ объявить локльную передачу"
+// var text_2 = "это способ объявить глобальную передачу"
+
+// console.log();
